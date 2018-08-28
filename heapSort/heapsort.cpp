184cfa22fa53ad3d1052d38f5f590e73e0bc73e5
alexandrejp1 <santacruzalexandre@gmail.com>
2018-08-28 17:31:26 -0300

    Create heapsort.cpp

---
+++ heapSort/heapsort.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+// criando um maxHeap
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;  // inicializando o maior como raiz
+    int l = 2*i + 1;  // esquerdo = 2*i + 1
+    int r = 2*i + 2;  // direito = 2*i + 2
+
+    // se o filho da esquerda é maior que a raiz (troca)
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+
+    // se o filho da direita é maior que a raiz
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    // quando o maior não estiver como a raiz
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+
+        // recursão na sub-árvore afetada pela troca
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n)
+{
+    // reorganizando o array na árvore
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // deletando o último nó da árvore depois que houver a troca entre o primeiro e o último
+    for (int i=n-1; i>=0; i--)
+    {
+        // trocando o primeiro com o último
+        swap(arr[0], arr[i]);
+
+        // chama heapify na árvore já reduzida
+        heapify(arr, i, 0);
+    }
+}
+
+int main()
+{
+    int vetor[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(vetor)/sizeof(vetor[0]);
+
+    heapSort(vetor, n);
+
+    cout << "\nArray ordenado: " << endl;
+    for(int x = 0; x < n; x++){
+        cout << vetor[x] << endl;
+    }
+   
+}
